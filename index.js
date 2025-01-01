1e8cc3963b0c7c7ffe6b3ccae94224ae4bbae06a
Pankajkumar2608 <125138274+Pankajkumar2608@users.noreply.github.com>
2025-01-01 18:15:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.post('/colleges', async (req, res) => {
     const collegeQuery = `
         SELECT "Location", "NIRF Rank", 
                "Highest Package (LPA)", "Average Package (LPA)", 
-               "Placement Rate %", "Facilities", "Reddit Review"
+               "Placement Rate (%)", "Facilities", "Reddit Review"
         FROM college_review 
         WHERE "College Name" = $1
         
