6f2572af9270192eb9314a389b1bcbd215843e29
Pankajkumar2608 <125138274+Pankajkumar2608@users.noreply.github.com>
2025-01-16 17:43:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const { Pool } = pkg;
 dotenv.config();
 const app = express();
 
-app.use(cors(
-    origin: ['https://www.motivationkaksha.com/','http://127.0.0.1:5577/','https://motivationkaksha.com/']
-));
+app.use(cors({
+  origin: ['https://www.motivationkaksha.com/', 'http://127.0.0.1:5577/', 'https://motivationkaksha.com/']
+}));
 
 app.use(express.json());
 
